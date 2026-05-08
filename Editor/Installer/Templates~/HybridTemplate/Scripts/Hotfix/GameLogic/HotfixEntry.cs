f1d74a8c22a49400dca8627e6773c0659113d0de
陈思海 <1464576565@qq.com>
2026-05-08 16:02:58 +0800

    feat: add hybrid framework installer template

---
+++ Editor/Installer/Templates~/HybridTemplate/Scripts/Hotfix/GameLogic/HotfixEntry.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Reflection;
+using AlicizaX;
+
+namespace GameLogic
+{
+    public static class HotfixEntry
+    {
+        private static List<Assembly> _hotfixAssembly;
+        public static List<Assembly> HotfixAssembly => _hotfixAssembly;
+
+        public static void Entrance(object[] objects)
+        {
+            Log.Info("HotFix Logic Entry!");
+            _hotfixAssembly = (List<Assembly>)objects[0];
+        }
+    }
+}
