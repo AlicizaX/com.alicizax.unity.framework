7348bff2f17b037e55a1271e396fbe867a779919
陈思海 <1464576565@qq.com>
2026-05-08 16:02:14 +0800

    feat: add ABase struct utilities

---
+++ Runtime/ABase/Structs/ObjectDontDestroyOnLoad.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace AlicizaX
+{
+    /// <summary>
+    /// 标记物体对象为不可销毁
+    /// </summary>
+    public sealed class ObjectDontDestroyOnLoad : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+    }
+}
